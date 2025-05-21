1611c84318c738fc663bd950af3ccd3df1013c6d
axoisaxo <133877250+AxoIsAxo@users.noreply.github.com>
2025-05-21 19:32:00 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,13 +20,13 @@ import {
 
 // --- Your web app's Firebase configuration ---
 const firebaseConfig = {
-  apiKey: "AIzaSyDoo957xiZB2heugMso-C6oS1jSjciLUq0",
-  authDomain: "maroltingervote.firebaseapp.com",
-  projectId: "maroltingervote",
-  storageBucket: "maroltingervote.appspot.com",
-  messagingSenderId: "536390639151",
-  appId: "1:536390639151:web:3f96ba960920552ecdb0da",
-  measurementId: "G-4TRS7E6NGK"
+    apiKey: "{{ env.FIREBASE_API_KEY }}",
+    authDomain: "{{ env.FIREBASE_AUTH_DOMAIN }}",
+    projectId: "{{ env.FIREBASE_PROJECT_ID }}",
+    storageBucket: "{{ env.FIREBASE_STORAGE_BUCKET }}",
+    messagingSenderId: "{{ env.FIREBASE_MESSAGING_SENDER_ID }}",
+    appId: "{{ env.FIREBASE_APP_ID }}",
+    measurementId: "{{ env.FIREBASE_MEASUREMENT_ID }}"
 };
 
 // --- Initialize Firebase ---
